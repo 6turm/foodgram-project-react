cd5b8b22f01a9904e551cd8f08cded4d3be0eda0
Roman Karimov <woodcamera@gmail.com>
2021-05-20 01:23:00 +0300

    create recipe.views.index()

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Recipe
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+def index(request):
+    recipe_list = Recipe.objects.all()  # select_related('имя связи') - для кэширования связанных объектов
+    paginator = Paginator(recipe_list, 6)
+    page_number = request.GET.get('page')
+    page = paginator.get_page(page_number)
+    return render(
+        request,
+        index.html,
+        {'page': page, 'paginator': paginator}
+    )
