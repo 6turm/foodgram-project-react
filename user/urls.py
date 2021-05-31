7172e90370e8bdc56596c2d0a7f4a04a744ba7a6
Roman Karimov <woodcamera@gmail.com>
2021-05-31 03:03:59 +0300

    redirect from login for auth users

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,13 @@
 from django.urls import include, path
+from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path(
+        'login/',
+        LoginView.as_view(redirect_authenticated_user=True),
+        name='login'
+    ),
     path('', include('django.contrib.auth.urls')),
 ]
