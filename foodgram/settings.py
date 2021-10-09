edf748f90396e9e20e28317d7c5b44c37b07f4c9
6turm <woodcamera@gmail.com>
2021-10-10 01:05:50 +0300

    change MEDIA to media

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [BASE_DIR / 'staticfiles']
-MEDIA_URL = '/MEDIA/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
