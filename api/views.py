d784ca3b271bd737e0a6054536dc65d4e1bd8aea
6turm <woodcamera@gmail.com>
2021-10-11 14:25:15 +0300

    add IsAuthenticated permisions

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.http.response import JsonResponse
 from rest_framework import status
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -11,6 +13,7 @@ from .serializers import (FavoriteSerializer, FollowSerializer,
 
 class AddToFavorites(APIView):
 
+    @permission_classes([IsAuthenticated])
     def post(self, request, format=None):
         serializer = FavoriteSerializer(data=request.data)
         if serializer.is_valid():
@@ -26,12 +29,14 @@ class AddToFavorites(APIView):
 
 
 class RemoveFromFavorites(APIView):
+    @permission_classes([IsAuthenticated])
     def delete(self, request, pk, format=None):
         Favorite.objects.filter(user=request.user, recipe_id=pk).delete()
         return Response({'success': True}, status=status.HTTP_200_OK)
 
 
 class Subscribe(APIView):
+    @permission_classes([IsAuthenticated])
     def post(self, request, format=None):
         serializer = FollowSerializer(data=request.data)
         if serializer.is_valid():
@@ -47,12 +52,14 @@ class Subscribe(APIView):
 
 
 class UnSubscribe(APIView):
+    @permission_classes([IsAuthenticated])
     def delete(self, request, pk, format=None):
         Follow.objects.filter(user=request.user, author_id=pk).delete()
         return Response({'success': True}, status=status.HTTP_200_OK)
 
 
 class GetProducts(APIView):
+    @permission_classes([IsAuthenticated])
     def get(self, request, format=None):
         serializer = ProductSerializer(data=request.query_params)
         if serializer.is_valid():
@@ -71,6 +78,7 @@ class GetProducts(APIView):
 
 
 class AddPurchase(APIView):
+    @permission_classes([IsAuthenticated])
     def post(self, request, format=None):
         serializer = OrderListSerializer(data=request.data)
         if serializer.is_valid():
@@ -86,6 +94,7 @@ class AddPurchase(APIView):
 
 
 class RemovePurchase(APIView):
+    @permission_classes([IsAuthenticated])
     def delete(self, request, pk, format=None):
         OrderList.objects.filter(user=request.user, recipe_id=pk).delete()
         return Response({'success': True}, status=status.HTTP_200_OK)
